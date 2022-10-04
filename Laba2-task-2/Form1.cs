fcc5d22fec34c0f4bf55a2969e326ae256ae205d
Константин Литвинов <litaistopet@icloud.com>
2022-10-04 17:31:19 +0800

    Добавьте файлы проекта.

---
+++ Laba2-task-2/Form1.cs
@@ -0,0 +1,46 @@
+namespace Laba2_task_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            txtInputString.Text = Properties.Settings.Default.sentence.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string sentence;
+            try
+            {
+              sentence = this.txtInputString.Text;
+            }
+            catch
+            {
+                return;
+            }
+            Properties.Settings.Default.sentence = sentence;
+            Properties.Settings.Default.Save();
+            MessageBox.Show(Logic.GetStringsReverse(sentence));
+        }
+    }
+    public class Logic
+    {
+        public static string GetStringsReverse(string sentence)
+        {
+            string reversed = "";
+            var words = sentence.Split(' ');
+            foreach (var word in words)
+            {
+                for (int i = word.Length - 1; i >= 0; i--)
+                {
+                    reversed += word[i];
+                }
+
+                reversed += " ";
+            }
+
+            return reversed.Trim();
+        }
+    }
+}
